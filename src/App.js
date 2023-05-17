e1a2b3ef807a5f524665a8cbb096a6d838854713
Bablukmr <bablukmr800@gmail.com>
2023-05-18 00:13:36 +0530

    Solution of Quiz App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
-import TodoList from './Components/folder-structure/Todo-list';
+import Quiz from './Quiz-Application/Quiz';
+
 
 function App() {
   return (
-    <div className="App">
-     <TodoList/>
+    <div >
+     <Quiz/>
     </div>
   );
 }
---
+++ src/Quiz-Application/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question: "Today is Monday, then what will be the day after 63 days?",
+        options:[ "Tuesday", "Monday", "Thursday", "Sunday"],
+        answer: 2
+    },
+    {
+        question: "Which one of the following is not a prime number?",
+        options:[ "31", "61", "71", "91"],
+        answer: 4
+    },
+    {
+        question: "Find the H.C.F, if the numbers are in the ratio of 4 : 5 : 6 and their L.C.M. is 2400.",
+        options:[ "35", "20", "40", "67"],
+        answer: 2
+    },
+    {
+        question: "The rate at which a sum becomes four times of itself in 25 years at S.I.,will be:",
+        options:[ "30%", "25%", "12%", "15%"],
+        answer: 3
+    },
+    {
+        question: "What is the average of first five multiples of 12?",
+        options:[ "36", "38", "40", "42"],
+        answer: 1
+    },
+    {
+        question: "If January 1, 1996, was Monday, what day of the week was January 1, 1997?",
+        options:[ "Thursday", "Wednesday", "Friday", "Sunday"],
+        answer: 2
+    },
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+];
